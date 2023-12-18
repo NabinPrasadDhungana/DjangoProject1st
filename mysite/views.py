a0b5b97c069554d05ff17ded1b413710a08aabed
NabinPrasadDhungana <prnabin123@gmail.com>
2023-12-18 21:30:20 +0545

    First commited mysite

---
+++ mysite/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+def homePage(request):
+    data={
+        'title':'Home | Mywebsite'
+    }
+    return render(request,"index.html",data)
+
+def interests(request):
+    return render(request,"interest.html")
+
+def contacts(request):
+    return render(request,"contacts.html")
