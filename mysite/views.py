549d713536865766ed7a258b06e68af0d0b7872f
NabinPrasadDhungana <prnabin123@gmail.com>
2023-12-19 19:19:27 +0545

    if else and for loop done

--- mysite/views.py
+++ mysite/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 def homePage(request):
     data={
-        'title':'Home | Mywebsite'
+        'title':'Home | Mywebsite',
+        'mlist':['Ram','Hari','Krishna',"Bishnu"]
     }
     return render(request,"index.html",data)
 
@@ -12,4 +13,10 @@ def contacts(request):
     return render(request,"contacts.html")
 
 def gallery(request):
-    return render(request,"gallery.html")
+    data={
+        'numbers':[5,10,20,40,80,160],
+        'details':[
+        {'name':'Nabin Prasad Dhungana','phone':9863000000},
+        {'name':'Prahari','phone':9843090901}]
+    }
+    return render(request,"gallery.html",data)
